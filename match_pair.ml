a142020b81be52e240dce3f4d35c8757189e50fb
hajime <paneda.youji.4696@gmail.com>
2017-01-07 15:09:58 +0900

    match2

--- match_pair.ml
+++ match_pair.ml
@@ -1,9 +1,8 @@
 (*目的：２つの整数の組pairを受け取り、その要素の和を返す*)
 (*add : int * int -> *)
-let add pair = match pair with
-    (a,b) -> a + b
+let add pair = match pair with (a,b) -> a + b
 
 (*test*)
-let test 1 = add (0,0) = 0
-let test 2 = add (3,5) = 8
-let test 3 = add (3,-5) = -2
+let test1 = add (0,0) = 0
+let test2 = add (3,5) = 8
+let test3 = add (3,-5) = -2
