85ade946687fb87affba78f7abe272a5761e5579
hajime <paneda.youji.4696@gmail.com>
2017-01-10 16:34:42 +0900

    book_t1

---
+++ book_t.ml
@@ -0,0 +1,13 @@
+type book_t = {
+    title : string;
+    author : string;
+    publisher : string;
+    price : int;
+    ISBN : string;
+}
+
+{title = "dragon";
+ author = "sunadori";
+ publisher = "shougakukan";
+ price = 3000;
+ ISVN = "23brht3"}
